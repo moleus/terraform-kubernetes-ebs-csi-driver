38746af758cb789eeea12455e1fc755f5a3f7e1a
Moleus <fafufuburr@gmail.com>
2025-03-06 13:51:47 +0300

    missed sa resource

--- node-rbac.tf
+++ node-rbac.tf
@@ -1,21 +1,9 @@
-#- labels           = {} -> null
-#- name             = "ebs-csi-node" -> null
-
 removed {
   from = kubernetes_service_account.node
   lifecycle {
     destroy = true
   }
 }
-resource "kubernetes_service_account" "node" {
-  metadata {
-    name      = local.daemonset_name
-    namespace = var.namespace
-    labels    = var.labels
-  }
-  automount_service_account_token = true
-}
-
 removed {
   from = kubernetes_cluster_role.node
   lifecycle {
